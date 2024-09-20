1e2f0a6ffaec9694242e59f16d35f6e0acaa05f2
Chase-Scripts <112039422+vk-chase@users.noreply.github.com>
2024-09-20 03:12:28 -0500

    Update wheelchair_cl.lua
    remove the qbcore = . 
    combine menus.
    move updatePlayerJob outside event.
    replaced Citizen.Wait with Wait()

--- c_Rx/client/wheelchair_cl.lua
+++ c_Rx/client/wheelchair_cl.lua
@@ -1,69 +1,128 @@
 local QBCore = exports['qb-core']:GetCoreObject()
-CreateThread(function()
-    exports['qb-target']:AddBoxZone("c_rentwchair", Config.RentTarget,1.8, 1,  {
-      name = "c_rentwchair",
-      heading = 249,
-      debugPoly = Config.Debug,
-      minZ = Config.RentTarget-1,
-      maxZ = Config.RentTarget+1,
+
+local spawnedPeds = {}
+local PlayerJob = {}
+local hasChairout = false
+
+local function deleteSpawnedPeds()
+    for _, ped in ipairs(spawnedPeds) do
+        if DoesEntityExist(ped) then
+            DeleteEntity(ped)
+        end
+    end
+    spawnedPeds = {}
+end
+
+local function spawnPed(pedData)
+    RequestModel(pedData.model)
+    while not HasModelLoaded(pedData.model) do
+        Wait(0)
+    end
+
+    local ped = CreatePed(0, pedData.model, pedData.coords.x, pedData.coords.y, pedData.coords.z - 1.0, pedData.coords.a, false, false)
+    SetEntityInvincible(ped, pedData.invincible)
+    FreezeEntityPosition(ped, pedData.freeze)
+    TaskStartScenarioInPlace(ped, pedData.scenario, -1, true)
+    SetBlockingOfNonTemporaryEvents(ped, true)
+    SetEntityNoCollisionEntity(ped, PlayerPedId(), false)
+
+    exports['qb-target']:AddCircleZone(pedData.name, vector3(pedData.coords.x, pedData.coords.y, pedData.coords.z), 2.0, {
+        name = pedData.name,
+        debugPoly = false,
+        useZ = true,
     }, {
-      options = {
-        {
-          type = "client",
-          event = "c_Rx:client:menu",
-          icon = 'fas fa-wheelchair',
-          label = 'Medical Aid',
-        },
-      },
-      distance = 2.5,
+        options = pedData.target.options,
+        distance = pedData.target.distance,
     })
+
+    table.insert(spawnedPeds, ped)
+end
+
+local function updatePlayerJob()
+    QBCore.Functions.GetPlayerData(function(PlayerData)
+        PlayerJob = PlayerData.job
+    end)
+end
+
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded', updatePlayerJob)
+RegisterNetEvent('QBCore:Client:OnJobUpdate', function(JobInfo)
+    PlayerJob = JobInfo
 end)
+
+AddEventHandler('onResourceStart', function(resource)
+    if GetCurrentResourceName() ~= resource then return end
+    updatePlayerJob()
+end)
+
+CreateThread(function()
+    deleteSpawnedPeds()
+
+    local ped1Data = {
+        name = 'emsarmory1',
+        model = 's_m_m_doctor_01',
+        coords = Config.RentTarget,
+        freeze = true,
+        invincible = true,
+        scenario = 'WORLD_HUMAN_CLIPBOARD',
+        target = {
+            options = {
+                {
+                    type = "client",
+                    event = "c_Rx:client:menu",
+                    icon = 'fas fa-wheelchair',
+                    label = 'Medical Aid',
+                },
+            },
+            distance = 3,
+        },
+    }
+    spawnPed(ped1Data)
+end)
+
+AddEventHandler('onResourceStop', function(resource)
+    if resource ~= GetCurrentResourceName() then return end
+    deleteSpawnedPeds()
+end)
+
+local function openMenu(menuItems)
+    exports['qb-menu']:openMenu(menuItems)
+end
 
 RegisterNetEvent('c_Rx:client:menu', function()
-  exports['qb-menu']:openMenu({
-      {
-          id = 1,
-          header = "REQUEST HOSPITAL EQUIPMENT",
-          isMenuHeader = true,
-      },
-      {
-        id = 2,
-        header = "REQUEST WHEELCHAIR",
-        params = {
-            event = "c_Rx:client:RentIt",
-            args = {
-                vehicle = 'iak_wheelchair',
-                
+    local menuItems = {
+        {
+            id = 1,
+            header = hasChairout and "RETURN HOSPITAL EQUIPMENT" or "REQUEST HOSPITAL EQUIPMENT",
+            isMenuHeader = true,
+        },
+        {
+            id = 2,
+            header = hasChairout and "RETURN WHEELCHAIR" or "REQUEST WHEELCHAIR",
+            params = {
+                event = hasChairout and "c_Rx:client:RemoveWChair" or "c_Rx:client:RentIt",
+                args = hasChairout and {} or { vehicle = 'iak_wheelchair' }
             }
-        }
-      },
-      {
-          id = 3,
-          header = "RETURN WHEELCHAIR",
-          params = {
-              event = "c_Rx:client:RemoveWChair"
-          }
-      },
-      {
-          id = 6,
-          header = "< Close Menu",
-          params = {
-              event = "qb-menu:closeMenu"
-          }
-      },
-  })
+        },
+        {
+            id = 3,
+            header = "< Close Menu",
+            params = {
+                event = "qb-menu:closeMenu"
+            }
+        },
+    }
+    openMenu(menuItems)
 end)
 
-RegisterNetEvent('c_Rx:client:RemoveWChair')
-AddEventHandler('c_Rx:client:RemoveWChair', function()
+RegisterNetEvent('c_Rx:client:RemoveWChair', function()
     QBCore.Functions.Notify('Wheelchair Returned!')
-    local car = GetVehiclePedIsIn(PlayerPedId(),true)
+    local car = GetVehiclePedIsIn(PlayerPedId(), true)
     DeleteVehicle(car)
     DeleteEntity(car)
+    hasChairout = false
 end)
 
-RegisterNetEvent('c_Rx:client:RentIt')
-AddEventHandler('c_Rx:client:RentIt', function(rental)
+RegisterNetEvent('c_Rx:client:RentIt', function(rental)
     local vehicle = rental.vehicle
     DoScreenFadeOut(1000)
     Wait(1500)
@@ -71,11 +130,12 @@ AddEventHandler('c_Rx:client:RentIt', function(rental)
     QBCore.Functions.Notify('Return at counter when you no longer need the wheelchair.')
     QBCore.Functions.SpawnVehicle(vehicle, function(veh)
         SetVehicleNumberPlateText(veh, "HOSP"..tostring(math.random(1000, 9999)))
-        exports[Config.FuelType]:SetFuel(veh, 100.0) -- if you dont wanna exclude wheelchair in fuel usage
-        TaskWarpPedIntoVehicle(GetPlayerPed(-1), veh, -1)
+        exports[Config.FuelType]:SetFuel(veh, 100.0)
+        TaskWarpPedIntoVehicle(PlayerPedId(), veh, -1)
         TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(veh))
         SetVehicleEngineOn(veh, true, true)
     end, coords, true)    
-    Citizen.Wait(300)
+    Wait(300)
+    hasChairout = true
     DoScreenFadeIn(2500) 
 end)
