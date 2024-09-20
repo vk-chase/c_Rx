00602403984579707e9e959bdcb3222acf0ebb0a
Chase-Scripts <112039422+vk-chase@users.noreply.github.com>
2024-09-20 03:16:07 -0500

    Update fxmanifest.lua
    clean up

--- c_Rx/fxmanifest.lua
+++ c_Rx/fxmanifest.lua
@@ -1,27 +1,30 @@
-this_is_a_map 'yes'
-fx_version "cerulean"
-game "gta5"
+fx_version 'cerulean'
+game 'gta5'
 lua54 'yes'
-server_scripts {'server/*.lua'}
-shared_scripts {'config.lua'}
+this_is_a_map 'yes'
+
+author 'vk-chase'
+version '1.1'
+description 'Prescription Drug System'
+
+shared_script 'config.lua'
 
 client_scripts {
-'client/*.lua'
-,'@PolyZone/client.lua'
-,'@PolyZone/CircleZone.lua'
+    'client/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
 }
 
 files {
-	'data/vehicles.meta',
-	'data/carvariations.meta',
-	'data/handling.meta'
+    'data/vehicles.meta',
+    'data/carvariations.meta',
+    'data/handling.meta'
 }
 
-data_file 'HANDLING_FILE'			'data/handling.meta'
-data_file 'VEHICLE_METADATA_FILE'	'data/vehicles.meta'
-data_file 'VEHICLE_VARIATION_FILE'	'data/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
 
---thank iak for the wheelchair, its not mine. provided to save you the DL and install time, go follow him on github.
-
-
-
+-- thank iak for the wheelchair, its not mine. provided to save you the DL and install time, go follow him on github.
